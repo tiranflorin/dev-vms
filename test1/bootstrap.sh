362f9e2edbf50119675b228c2ea94b4b91445a7f
tiranflorin <tiran.florin@gmail.com>
2016-03-31 00:55:36 +0300

    Configured basic provisioning using a shell script (Apache, Php, MySql).

---
+++ test1/bootstrap.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+PASSWORD='12345678'
+PROJECT_FOLDER='test1-folder'
+
+# update / upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# install apache 2.5 and php 5.5
+sudo apt-get install -y apache2
+sudo apt-get install -y php5
+
+# install mysql and give password to installer
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo apt-get -y install mysql-server
+sudo apt-get install php5-mysql
+
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html/${PROJECT_FOLDER}"
+    <Directory "/var/www/html/${PROJECT_FOLDER}">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# restart apache
+service apache2 restart
+
+# install git
+sudo apt-get -y install git
+
+# install Composer
+curl -s https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
